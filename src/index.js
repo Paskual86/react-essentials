7eda60856b40758535ea2f81c9a2f004a1243435
Pablo Sebastian Andrada <pablosebastian.andrada@gmail.com>
2021-03-13 11:08:33 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,26 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
-function AppTwo(){
-  return <h1>This is the Second App</h1>;
-}
-
-{/*
-ReactDOM.render(
-  <React.Fragment>
-    <App />
-    <AppTwo/>
-  </React.Fragment>,
-  document.getElementById('root')
-);
-
-*/}
-
 ReactDOM.render(
   <React.StrictMode>
     <App />
-    <AppTwo/>
   </React.StrictMode>,
   document.getElementById('root')
 );
