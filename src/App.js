36dc6bc52c96bf2b96ed4bb2e90d761e8781f375
Paskual2014 <pablo.andrada@kinandcarta.com>
2021-03-17 12:24:21 -0300

    React State in component Tree - Conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,53 +1,21 @@
 import './App.css';
 import restaurant from './FoodImage1.jpg'
-function Header(props){
-  return (
-    <header>
-      <h1>La cocina de {props.name}</h1>
-    </header>
-  )
+
+function GrantedComponent(){
+  return <div>Granted component</div>
 }
 
-function Main(props){
-  return (
-    <section>
-      <p>Bienvenidos al {props.adjetives} lugar de comida saludable</p>
-      <img src={restaurant} alt="restaurant"></img>
-      { /*<img src="http://github.com/Paskual86.png" alt="restaurant"></img>*/}
-      <ul style={{textAlign:"Left"}}>
-        {props.dishes.map( (dish)=> (
-          <li key={dish.id}>{dish.title}</li>
-        ))}
-      </ul>
-    </section>
-  )
+function EveryoneComponent(){
+  return <div>Everyone component</div>
 }
 
-function Footer(props){
-  return (
-    <footer>
-      <h3>Derechos {props.year}</h3>
-    </footer>
-  )
-}
+function App(props) {
 
-const dishes = [
-  "Macaroni and cheese",
-  "Salmon",
-  "Tofu with vegetables"
-];
-
-//dishes.map((dish) => console.log(dish));
-const dishObjects = dishes.map((dish, i) => ({id:i, title:dish}));
-
-function App() {
-  return (
-    <div className="App">
-      <Header name="Pablo"/>
-      <Main adjetives="Mejor" dishes={dishObjects}/>
-      <Footer year={new Date().getFullYear()}/>
-    </div>
-  );
+  if(props.authorize){
+    return GrantedComponent();
+  }else{
+    return EveryoneComponent();
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App authorize={true} />
   </React.StrictMode>,
   document.getElementById('root')
 );
