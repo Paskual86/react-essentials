e5cbbe4cdff76abb56d9ff40bc454c0a6163b1b5
Pablo Sebastian Andrada <pablosebastian.andrada@gmail.com>
2021-04-06 07:07:32 -0300

    add events to form

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,14 +1,33 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
 function Formulario () {
+    const [nombre, setNombre] = useState('');
+    const [edad, setEdad] = useState('');
+
+    const Validar = (event) =>{
+        event.preventDefault();
+        console.log("Button Press");
+        if(!nombre.trim()){
+            console.log('El nombre esta vacio');
+            return;
+        }
+        if(!edad.trim()){
+            console.log('Edad esta vacio');
+            return;
+        }
+    }
     return (
         <div className="container" >
-            <form className="form-group">
+            <form onSubmit={Validar} className="form-group">
                 <input type="text"
                     className="form-control mb-3"
+                    placeholder="Nombre"
+                    onChange={(e) => {setNombre(e.target.value)}}
                 />
                 <input type="text"
                 className="form-control mb-3"
+                placeholder="Edad"
+                onChange={(e) => {setEdad(e.target.value)}}
                 />
                 <input type="submit"
                 className="btn btn-info btn-block mb-3"
