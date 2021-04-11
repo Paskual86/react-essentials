2176fd210b88246de31c25830d987820b6a9f181
Pablo Sebastian Andrada <pablosebastian.andrada@gmail.com>
2021-04-10 22:44:38 -0300

    Add routing

--- src/App.js
+++ src/App.js
@@ -2,15 +2,33 @@ import './App.css';
 import ListadoNombres from './components/ListadoNombres'
 import Welcome from './components/Welcome';
 import Card from './components/Card';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-     <ListadoNombres></ListadoNombres>
-     <Welcome name="Pablo"></Welcome>
-     <Welcome name="Noe"></Welcome>
-     <Card imagen="https://picsum.photos/id/237/200/300" titulo="Primer Imagen" texto="Imagen Predeterminada"></Card>
-    </div>
+    <Router>
+      <Link to="/">Base</Link>
+      <Link to="/inicio">Card</Link>
+      <Link to="/listado">Listado de Nombres</Link>
+
+      <Switch>
+        <Route path="/inicio">
+          <Welcome name="Pablo"></Welcome>
+          <Welcome name="Noe"></Welcome>
+        </Route>
+        <Route path="/listado">
+          <ListadoNombres></ListadoNombres>
+        </Route>
+        <Route path="/">
+          <Card imagen="https://picsum.photos/id/237/200/300" titulo="Primer Imagen" texto="Imagen Predeterminada"></Card>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
