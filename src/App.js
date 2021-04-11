0f7790013c30254c52fff1c9caf8f6de9f898d20
Pablo Sebastian Andrada <pablosebastian.andrada@gmail.com>
2021-04-10 22:13:16 -0300

    manage props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
 import ListadoNombres from './components/ListadoNombres'
+import Welcome from './components/Welcome';
+import Card from './components/Card';
 
 function App() {
   return (
     <div className="App">
      <ListadoNombres></ListadoNombres>
+     <Welcome name="Pablo"></Welcome>
+     <Welcome name="Noe"></Welcome>
+     <Card imagen="https://picsum.photos/id/237/200/300" titulo="Primer Imagen" texto="Imagen Predeterminada"></Card>
     </div>
   );
 }
